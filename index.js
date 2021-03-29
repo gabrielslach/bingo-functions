281bb4194c5b449fb0706bfda4aa15d1f84861d0
gabrielslach <gabdl.dev@gmail.com>
2021-03-29 11:37:35 +0800

    Fixed views

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const functions = require('firebase-functions');
 const express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+const cors = require('cors')({origin: true});
 var createError = require('http-errors');
+var path = require('path');
 
 var getPlayerRouter = require('./routes/getPlayer');
 var getPlayerAllRouter = require('./routes/getPlayerAll');
@@ -20,25 +21,28 @@ const app = express();
 
 var db = firebaseAdmin();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/',cors({origin: true}));
-app.use('/login',login(db));
-app.use('/player-login',playerLogin(db));
-app.use('/api',jwtAuth);
-app.use('/api/get-player', getPlayerRouter(db));
-app.use('/api/get-player-all', getPlayerAllRouter(db));
-app.use('/api/register-player', registerPlayerRouter(db));
-app.use('/api/pick-cell', pickCellRouter(db));
-app.use('/api/get-picked-cells', getPickCellsRouter(db));
+app.use('/app/',cors);
+app.use('/app/login',login(db));
+app.use('/app/player-login',playerLogin(db));
+app.use('/app/api',jwtAuth);
+app.use('/app/api/get-player', getPlayerRouter(db));
+app.use('/app/api/get-player-all', getPlayerAllRouter(db));
+app.use('/app/api/register-player', registerPlayerRouter(db));
+app.use('/app/api/pick-cell', pickCellRouter(db));
+app.use('/app/api/get-picked-cells', getPickCellsRouter(db));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
-  });
+  next(createError(404));
+});
 
   // error handler
 app.use(function(err, req, res, next) {
